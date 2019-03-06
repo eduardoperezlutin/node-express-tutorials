a89d93c9f572487c87e389fcaef4ca79f9f1ee3c
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-05 23:43:10 -0600

    nodemon config

--- pluralsight-library/app.js
+++ pluralsight-library/app.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -16,6 +18,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, () => {
-  debug(`Ready and listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+  debug(`Ready! and listening on port ${chalk.green(port)}`);
 });
