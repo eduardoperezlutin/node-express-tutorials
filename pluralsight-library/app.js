114ddb67b05eb332beec1bfab22b559765a22f89
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-11 11:20:29 -0600

    Separate files | routes folder

--- pluralsight-library/app.js
+++ pluralsight-library/app.js
@@ -6,7 +6,6 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -18,58 +17,8 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-const books = [
-  {
-    title: 'War and Peace',
-    genre: 'Historical Fiction',
-    author: 'Lev Nikolayevich Tolstoy',
-    read: false
-  },
-  {
-    title: 'Les Miserables',
-    genre: 'Historical Fiction',
-    author: 'Victor Hugo',
-    read: false
-  },
-  {
-    title: 'The Time Machine',
-    genre: 'Science Fiction',
-    author: 'H. G. Wells',
-    read: false
-  },
-  {
-    title: 'A Journey into the Center of the Earth',
-    genre: 'Science Fiction',
-    author: 'Jules Verne',
-    read: false
-  }
-];
-
-bookRouter.route('/')
-  .get((req, res) => {
-    res.render(
-      'books',
-      { 
-        nav: [
-          {
-            link: '/books',
-            title: 'Books'
-          },
-          {
-            link: '/authors',
-            title: 'Authors'
-          }
-        ],
-        title: 'Books',
-        books
-      }
-    );
-  });
-
-bookRouter.route('/single')
-  .get((req, res) => {
-    res.send('hello single book');
-  });
+// routes
+const bookRouter = require('./src/routes/bookRoutes')
 
 app.use('/books', bookRouter);
 app.get('/', (req, res) => {
---
+++ pluralsight-library/src/routes/bookRoutes.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bookRouter = express.Router();
+
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'A Journey into the Center of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  }
+];
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render(
+      'books', 
+      {
+        nav: [
+          {
+            link: '/books',
+            title: 'Books'
+          },
+          {
+            link: '/authors',
+            title: 'Authors'
+          }
+        ],
+        title: 'Books',
+        books
+      }
+    );
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+module.exports = bookRouter;
