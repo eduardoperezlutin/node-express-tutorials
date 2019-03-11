9cf887d6ea187053aed99eec6e2c38dc5f73c90e
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-11 17:15:51 -0600

    Create Mongodb and Insert books

--- pluralsight-library/src/routes/adminRoutes.js
+++ pluralsight-library/src/routes/adminRoutes.js
@@ -1,11 +1,58 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
 const adminRouter = express.Router();
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'A Journey into the Center of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  }
+];
 
 function router(nav) {
 
   adminRouter.route('/')
     .get((req, res) => {
-      res.send('inserting books');
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url, { useNewUrlParser: true });
+          debug('Connected successfully to server');
+
+          const db = client.db(dbName);
+
+          const response = await db.collection('books').insertMany(books);
+          res.json(response);
+
+        } catch (error) {
+          debug(error.stack);
+        }
+
+        client.close();
+      }());
     })
   
   return adminRouter;
