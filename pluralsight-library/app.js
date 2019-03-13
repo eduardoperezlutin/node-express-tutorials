7f14f76730476a8ba8aeda8e4dd396b9e2a095cc
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-13 14:54:40 -0600

    Auth routes

--- pluralsight-library/app.js
+++ pluralsight-library/app.js
@@ -3,11 +3,14 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/public/')));
 
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -32,9 +35,11 @@ const nav = [
 // routes
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
   res.render(
