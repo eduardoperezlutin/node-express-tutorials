5ecc0b6cb18d4555b49d834768e2b7022e23cbd1
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-13 20:12:19 -0600

    Create user in DB

--- pluralsight-library/src/routes/authRoutes.js
+++ pluralsight-library/src/routes/authRoutes.js
@@ -7,19 +7,45 @@ const authRouter = express.Router();
 function router() {
   authRouter.route('/signUp')
     .post((req, res) => {
-      debug(req.body);
-      // create user
-      req.login(req.body, () => {
-        res.redirect('/auth/profile');
-      })
+      const { username, password } = req.body;
+
+      // database
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function addUser() {
+        let client;
+
+        try {
+          // db conection
+          client = await MongoClient.connect(url);
+          debug('Connected to server successfully!');
+
+          // db & collection selection
+          const db = client.db(dbName);
+          const col = db.collection('users');
+
+          // create & insert user
+          const user = { username, password };
+          const results = await col.insertOne(user);
+
+          // login & return user
+          req.login(results.ops[0], () => {
+            res.redirect('/auth/profile');
+          });
+        } catch (error) {
+          debug('Error creating user in login');
+          debug(error.stack);
+        }
+      }());
     });
-  
+
   authRouter.route('/profile')
     .get((req, res) => {
       res.json(req.user);
-    })
+    });
 
-  return authRouter; 
+  return authRouter;
 }
 
 module.exports = router;
