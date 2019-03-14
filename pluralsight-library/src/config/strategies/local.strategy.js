39d7cd2bba64b77adc68f0419c022d0eacf43113
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-13 23:29:50 -0600

    Sign in routes, eslint, and preparation for user validation

--- pluralsight-library/src/config/strategies/local.strategy.js
+++ pluralsight-library/src/config/strategies/local.strategy.js
@@ -2,16 +2,16 @@ const passport = require('passport');
 const { Strategy } = require('passport-local');
 
 module.exports = function localStrategy() {
-    passport.use(new Strategy(
-        {
-            usernameField: 'username',
-            passwordField: 'password'
-        }, (username, password, done) => {
-            // db stuff
-            const user = {
-                username, password
-            };
-            done(null, user);
-        }
-    ));
-}
+  passport.use(new Strategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password'
+    }, (username, password, done) => {
+      // db stuff
+      const user = {
+        username, password
+      };
+      done(null, user);
+    }
+  ));
+};
--- pluralsight-library/src/routes/authRoutes.js
+++ pluralsight-library/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const debug = require('debug')('app:authRoutes');
+const passport = require('passport');
 
 const authRouter = express.Router();
 
-function router() {
+function router(nav) {
   authRouter.route('/signUp')
     .post((req, res) => {
       const { username, password } = req.body;
@@ -39,6 +40,26 @@ function router() {
         }
       }());
     });
+
+  authRouter.route('/signIn')
+    .get((req, res) => {
+      res.render(
+        'signin',
+        {
+          nav,
+          title: 'Sign In'
+        }
+      );
+    })
+
+    // manage sign in with passport
+    .post(passport.authenticate(
+      'local',
+      {
+        successRedirect: '/auth/profile',
+        failureRedirect: '/'
+      }
+    ));
 
   authRouter.route('/profile')
     .get((req, res) => {
